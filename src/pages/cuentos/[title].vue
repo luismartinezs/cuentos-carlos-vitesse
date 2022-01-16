5ff32b617e0d4e0a46eaac4ed53dede5462861ee
Luis Martinez <luis@passionatepeople.io>
2022-01-16 19:43:12 +0100

    First commit

---
+++ src/pages/cuentos/[title].vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import PapaNoel from '../../posts/papa-noel.md'
+// import { useUserStore } from '~/stores/user'
+
+// const props = defineProps<{ name: string }>()
+// const router = useRouter()
+// const user = useUserStore()
+// const { t } = useI18n()
+
+// watchEffect(() => {
+//   user.setNewName(props.title)
+// })
+</script>
+
+<template>
+  <div>
+    <PapaNoel />
+    <div>
+      <button
+        class="btn m-3 text-sm mt-6"
+        @click="router.back()"
+      >
+        {{ t('button.back') }}
+      </button>
+    </div>
+  </div>
+</template>
